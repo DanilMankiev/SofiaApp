26c2e0bfa0edabee728dd122ec2a76ecced0ccf9
danil.m <s3cidc@gmail.com>
2024-07-23 14:53:30 +0300

    complete rabbitmq connection

--- Notification/pkg/rabbitmq/errors.go
+++ Notification/pkg/rabbitmq/errors.go
@@ -6,4 +6,5 @@ var (
 	errNotConnected = errors.New("not connected to server")
 	errNotValidMsg = errors.New("not valid message") 
 	errShutDown = errors.New("client is shutting down")
+	errAlreadyClose = errors.New("already closed: not connected to server")
 )
